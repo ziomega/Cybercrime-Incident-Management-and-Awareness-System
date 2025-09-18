dc24e1a78eb476c686028a6f810dcfb0cd62b525
sinlessrook <asadithyan141@gmail.com>
2025-09-18 22:04:16 +0530

    add admin site

--- backend/CIMAS/awareness/admin.py
+++ backend/CIMAS/awareness/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
+from .models import CrimeTypes, Solutions
+
 # Register your models here.
+admin.site.register(CrimeTypes)
+admin.site.register(Solutions)
--- backend/CIMAS/incidents/admin.py
+++ backend/CIMAS/incidents/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
-
+from .models import Incidents, IncidentAssignments
 # Register your models here.
+admin.site.register(Incidents)
+admin.site.register(IncidentAssignments)
